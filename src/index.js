20d1828b5f33316d95b1681b9ca1cecf20a38249
Scott Lowe <scott@entercastle.com>
2015-05-18 02:33:25 -0400

    docs(EmailService): add documentation for constructor and methods

--- src/index.js
+++ src/index.js
@@ -1,13 +1,29 @@
 var q = require('q');
-
-var EmailService = function (emailProvider, storageService, fromEmail) {
-  if (!emailProvider) { throw new Error('Email provider required'); }
-  if (!storageService) { throw new Error('Storage service required'); }
-  if (!fromEmail) { throw new Error('From email address required'); }
-  var _provider = emailProvider;
-  var _storage = storageService;
-  var _from = fromEmail;
-
+/**
+ * A tool for sending emails
+ * @class EmailService
+ * @param {EmailProvider} provider the email provider which this service delegates to
+ * @param {StorageService} storage an instance of le-storage-service that is used to create records
+ * @param {string} from the email address of the sender
+ * @returns {service}
+ */
+var EmailService = function (provider, storage, from) {
+  if (!provider) { throw new Error('Email provider required'); }
+  if (!storage) { throw new Error('Storage service required'); }
+  if (!from) { throw new Error('From email address required'); }
+  var _provider = provider;
+  var _storage = storage;
+  var _from = from;
+  /**
+   * Sends an email with html content
+   * @function sendHtml
+   * @memberof EmailService
+   * @instance
+   * @param {string} to the email address of the recipient
+   * @param {string} subject the subject line of the email
+   * @param {string} html the html content
+   * @returns {record}
+   */
   this.sendHtml = function (to, subject, html) {
     if (!to) { return q.reject(new Error('To email required')); }
     if (!subject) { return q.reject(new Error('Subject required')); }
@@ -22,7 +38,16 @@ var EmailService = function (emailProvider, storageService, fromEmail) {
       }).then(function () { return record });
     });
   };
-
+  /**
+   * Sends a template email
+   * @function sendTemplate
+   * @memberof EmailService
+   * @instance
+   * @param {string} to the email address of the recipient
+   * @param {string} id the unique identifier of the template
+   * @param {Object} data the key/value pairs to inject
+   * @returns {record}
+   */
   this.sendTemplate = function (to, id, data) {
     if (!to) { return q.reject(new Error('To email required')); }
     if (!id) { return q.reject(new Error('Template ID required')); }
